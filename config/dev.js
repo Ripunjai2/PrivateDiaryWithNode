442b59af0f187b7953a3970908b1783345c5be6f
Ripunjai <ripunjai2@gmail.com>
2021-02-10 21:56:38 +0530

    All Boilerplate code and Basic add blog done

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://Ripunjai:04euuPT5vYOKoDSv@cluster0-shard-00-00.palh8.mongodb.net:27017,cluster0-shard-00-01.palh8.mongodb.net:27017,cluster0-shard-00-02.palh8.mongodb.net:27017/test?ssl=true&replicaSet=atlas-rokjmp-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
